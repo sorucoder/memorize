75263a1c4a3d340d19d7712e16e42d02846b0b7a
Marcus <mgermano79@Admins-Mac-mini.local>
2024-01-18 09:27:59 -0500

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Marcus on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
